db7be4a7e4f1a33c0ff242abf6395e7daa2b62f8
Joseph Abbott <joseph.william.abbott@gmail.com>
2023-03-17 00:37:41 +0100

    simplify run scripts with settings file

--- docs/example/run_scripts/run_data_split.py
+++ docs/example/run_scripts/run_data_split.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 import ase.io
 import numpy as np
 
@@ -7,58 +8,47 @@ from equistore import Labels
 
 from equisolve.utils import split_data
 
-import rholearn    # torch-based density leaning
 from rholearn import io, features, utils
-
-
-RHOLEARN_DIR = "/Users/joe.abbott/Documents/phd/code/rho/rho_learn/"  # for example
-data_dir = os.path.join(RHOLEARN_DIR, "docs/example/water/data")
+from settings import RASCAL_HYPERS, DATA_SETTINGS, ML_SETTINGS
 
 
 # Read the water molecules from file
 n_structures = 1000
-frames = ase.io.read(
-    os.path.join(data_dir, "water_monomers_1k.xyz"), index=f":{n_structures}"
+frames = ase.io.read(DATA_SETTINGS["xyz"], index=f":{n_structures}"
 )
-
-# Define and save rascaline hypers
-rascal_hypers = {
-    "cutoff": 3.0,  # Angstrom
-    "max_radial": 6,  # Exclusive
-    "max_angular": 5,  # Inclusive
-    "atomic_gaussian_width": 0.2,
-    "radial_basis": {"Gto": {}},
-    "cutoff_function": {"ShiftedCosine": {"width": 0.5}},
-    "center_atom_weight": 1.0,
-}
-io.pickle_dict(os.path.join(data_dir, "rascal_hypers.pickle"), rascal_hypers)
 
 # Compute lambda-SOAP: uses rascaline to compute a SphericalExpansion
 # Runtime approx 25 seconds
 input = features.lambda_soap_vector(
-    frames, rascal_hypers, even_parity_only=True
+    frames, RASCAL_HYPERS, even_parity_only=True
 )
 
-# Drop the block for l=5, Hydrogen as this isn't included in the output electron density
-input = equistore.drop_blocks(input, keys=Labels(input.keys.names, np.array([[5, 1]])))
-
-# Save lambda-SOAP descriptor to file
-equistore.save(os.path.join(data_dir, "lambda_soap.npz"), input)
-
 # Load the electron density data
-output = equistore.load(os.path.join(data_dir, "e_densities.npz"))
+output = equistore.load(os.path.join(DATA_SETTINGS["data_dir"], "e_densities.npz"))
+
+# Drop the block for l=5, Hydrogen as this isn't included in the output electron density
+input = equistore.drop_blocks(input, keys=np.setdiff1d(input.keys, output.keys))
 
 # Check that the metadata of input and output match along the samples and components axes
 assert equistore.equal_metadata(input, output, check=["samples", "components"])
 
+# Save lambda-SOAP descriptor to file
+equistore.save(os.path.join(DATA_SETTINGS["data_dir"], "lambda_soap.npz"), input)
+
+# Write settings to file
+with open(os.path.join(DATA_SETTINGS["data_dir"], "rascal_hypers.txt"), "w+") as f:
+    f.write(f"Rascal hypers:\n{pprint.pformat(RASCAL_HYPERS)}\n")
+with open(os.path.join(DATA_SETTINGS["data_dir"], "data_settings.txt"), "w+") as f:
+    f.write(f"Data settings:\n{pprint.pformat(DATA_SETTINGS)}\n")
+
 # Split the data into training, validation, and test sets
 [[in_train, in_test, in_val], [out_train, out_test, out_val]], grouped_labels = split_data(
     [input, output],
-    axis="samples",
-    names="structure",
-    n_groups=3,
-    group_sizes=[800, 199, 1],
-    seed=10,
+    axis=DATA_SETTINGS["axis"],
+    names=DATA_SETTINGS["names"],
+    n_groups=DATA_SETTINGS["n_groups"],
+    group_sizes=DATA_SETTINGS["group_sizes"],
+    seed=DATA_SETTINGS["seed"],
 )
 tm_files = {
     "in_train.npz": in_train,
@@ -70,4 +60,4 @@ tm_files = {
 }
 # Save the TensorMaps to file
 for name, tm in tm_files.items():
-    equistore.save(os.path.join(data_dir, name), tm)
+    equistore.save(os.path.join(DATA_SETTINGS["data_dir"], name), tm)
--- docs/example/run_scripts/run_training.py
+++ docs/example/run_scripts/run_training.py
@@ -7,72 +7,29 @@ import equistore
 
 import rholearn
 from rholearn import io, features, pretraining, training, utils
-
-RHOLEARN_DIR = "/Users/joe.abbott/Documents/phd/code/rho/rho_learn/"  # for example
-data_dir = os.path.join(RHOLEARN_DIR, "docs/example/water/data")
+from settings import DATA_SETTINGS, ML_SETTINGS
 
 
-run_dir = os.path.join(RHOLEARN_DIR, "docs/example/water/runs")
-io.check_or_create_dir(run_dir)
-
-settings = {
-    "io": {
-        "data_dir": os.path.join(data_dir),
-        "run_dir": os.path.join(run_dir, "00_nonlinear_1"),
-    },
-    "torch": {
-        "requires_grad": True,  # needed to track gradients
-        "dtype": torch.float64,  # recommended
-        "device": torch.device("cpu"),  # which device to load tensors to
-    },
-    "model": {
-        "type": "nonlinear",  # linear or nonlinear
-        "args": {
-            "hidden_layer_widths": [8, 8],
-            "activation_fn": "SiLU"
-        },
-    },
-    "optimizer": {
-        "algorithm": torch.optim.LBFGS,
-        "args": {
-            "lr": 0.25,
-        },
-    },
-    "loss": {
-        "fn": "MSELoss",  # CoulombLoss or MSELoss
-        "args": {
-            "reduction": "sum",  # reduction can be used with MSELoss
-        },
-    },
-    "training": {
-        "n_epochs": 50,  # number of total epochs to run
-        "save_interval": 50,  # save model and optimizer state every x intervals
-        "restart_epoch": None,  # None, or the epoch checkpoint number if restarting
-        "standardize_invariant_features": True,
-    },
-}
-# Save settings
-io.check_or_create_dir(settings["io"]["run_dir"])
-io.pickle_dict(os.path.join(settings["io"]["run_dir"], "train_settings.pickle"), settings)
-with open(os.path.join(settings["io"]["run_dir"], "train_settings.txt"), "w+") as f:
-    f.write(f"Settings:\n\n{pprint.pformat(settings)}")
+# Create simulation run directory and save simulation
+io.check_or_create_dir(ML_SETTINGS["run_dir"])
+with open(os.path.join(ML_SETTINGS["run_dir"], "ml_settings.txt"), "a+") as f:
+    f.write(f"ML Settings:\n{pprint.pformat(ML_SETTINGS)}\n\n")
 
 # IMPORTANT! - set the torch default dtype
-torch.set_default_dtype(settings["torch"]["dtype"])
+torch.set_default_dtype(ML_SETTINGS["torch"]["dtype"])
 
-# Construct the appropriate torch objects (i.e. models, loss fxns) prior to training
-# pretraining.construct_torch_objects(settings)
+# Pre-construct the appropriate torch objects (i.e. models, loss fxns)
 pretraining.construct_torch_objects_in_train_dir(
-    settings, settings["io"]["data_dir"], settings["io"]["run_dir"]
+    DATA_SETTINGS["data_dir"], ML_SETTINGS["run_dir"], ML_SETTINGS, 
 )
 
 # Define the training subdirectory
 train_rel_dir = ""
-train_run_dir = os.path.join(settings["io"]["run_dir"], train_rel_dir)
+train_run_dir = os.path.join(ML_SETTINGS["run_dir"], train_rel_dir)
 
 # Load training data and torch objects
 data, model, loss_fn, optimizer = pretraining.load_training_objects(
-    settings, train_rel_dir, settings["training"]["restart_epoch"]
+    train_rel_dir, DATA_SETTINGS["data_dir"], ML_SETTINGS, ML_SETTINGS["training"]["restart_epoch"]
 )
 
 # Unpack the data
@@ -87,8 +44,8 @@ training.train(
     model=model,
     loss_fn=loss_fn,
     optimizer=optimizer,
-    n_epochs=settings["training"]["n_epochs"],
-    save_interval=settings["training"]["save_interval"],
+    n_epochs=ML_SETTINGS["training"]["n_epochs"],
+    save_interval=ML_SETTINGS["training"]["save_interval"],
     save_dir=train_run_dir,
-    restart=settings["training"]["restart_epoch"],
+    restart=ML_SETTINGS["training"]["restart_epoch"],
 )
