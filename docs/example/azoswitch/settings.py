7583be942a09315ec7c03197665237b523781b7b
Joseph Abbott <joseph.william.abbott@gmail.com>
2023-03-17 17:35:48 +0100

    update settings

--- docs/example/azoswitch/settings.py
+++ docs/example/azoswitch/settings.py
@@ -6,7 +6,7 @@ RHOLEARN_DIR = "/Users/joe.abbott/Documents/phd/code/rho/rho_learn/"
 
 # Define the rascaline hypers for generating the lambda-SOAP features
 RASCAL_HYPERS = {
-    "cutoff": 3.0,  # Angstrom
+    "cutoff": 5.0,  # Angstrom
     "max_radial": 6,  # Exclusive
     "max_angular": 5,  # Inclusive
     "atomic_gaussian_width": 0.2,
@@ -18,19 +18,22 @@ RASCAL_HYPERS = {
 DATA_SETTINGS = {
     # Set path where the data should be stored
     "data_dir": os.path.join(RHOLEARN_DIR, "docs/example/azoswitch/data"),
-    "axis": "samples",         # which axis to split the data along
-    "names": ["structure"],    # what index to split the data by - i.e. "structure"
-    "n_groups": 3,             # num groups for data split (i.e. 3 for train-test-val)
-    "group_sizes": [500, 200, 1],  # the abs/rel group sizes for the data splits
-    "n_exercises": 2,  # the number of learning exercises to perform
-    "n_subsets": 3,    # how many subsets to use for each exercise
-    "seed": 10,        # random seed for data split
+    "coulomb_metrics": os.path.join(
+        RHOLEARN_DIR, "docs/example/azoswitch/data/coulomb_metrics.npz"
+    ),
+    "axis": "samples",  # which axis to split the data along
+    "names": ["structure"],  # what index to split the data by - i.e. "structure"
+    "n_groups": 3,  # num groups for data split (i.e. 3 for train-test-val)
+    "group_sizes": [7, 2, 1],  # the abs/rel group sizes for the data splits
+    "n_exercises": 1,  # the number of learning exercises to perform
+    "n_subsets": 3,  # how many subsets to use for each exercise
+    "seed": 10,  # random seed for data split
 }
 
 # Define ML settings
 ML_SETTINGS = {
     # Set path where the simulation should be run
-    "run_dir": os.path.join(RHOLEARN_DIR, "docs/example/azoswitch/runs/demo_linear"),
+    "run_dir": os.path.join(RHOLEARN_DIR, "docs/example/azoswitch/runs/01_linear"),
     "torch": {
         "requires_grad": True,  # needed to track gradients
         "dtype": torch.float64,  # recommended
@@ -56,8 +59,8 @@ ML_SETTINGS = {
         },
     },
     "training": {
-        "n_epochs": 25,  # number of total epochs to run
-        "save_interval": 25,  # save model and optimizer state every x intervals
+        "n_epochs": 10,  # number of total epochs to run
+        "save_interval": 10,  # save model and optimizer state every x intervals
         "restart_epoch": None,  # None, or the epoch checkpoint number if restarting
         "standardize_invariant_features": True,
     },
