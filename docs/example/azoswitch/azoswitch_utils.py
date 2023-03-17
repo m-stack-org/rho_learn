a32b46b0358ddad4399a2ce9df3a4899b186c4f3
Joseph Abbott <joseph.william.abbott@gmail.com>
2023-03-17 17:35:12 +0100

    slimmed utils

--- docs/example/azoswitch/azoswitch_utils.py
+++ docs/example/azoswitch/azoswitch_utils.py
@@ -4,60 +4,11 @@ A module containing helper functions specific to the example workflow.
 import os
 import numpy as np
 
-from equistore import io, Labels, TensorBlock, TensorMap
+import equistore
+from equistore import Labels, TensorMap
 
 
-def clean_azoswitch_lambda_soap(input: TensorMap) -> TensorMap:
-    """
-    Cleans the lambda-SOAP structural descriptor for the azoswitch database by
-    a) dropping all blocks with odd parity (i.e. sigma = -1), b) dropping the
-    block corresponding to lambda channel 5 and species Hydrogen, and c)
-    modifies the keys of the blocks such that the 'order_nu' name is omitted.
-    """
-    # Record at which index the key names appear in the input TensorMap key
-    # Labels, makes indexing agnostic to ordering.
-    key_idxs = {key_name: idx for idx, key_name in enumerate(input.keys.names)}
-
-    # Iterate over key: block pairs in the lambda-SOAP descriptor and store the
-    # indices of the keys that should be kept in the return TensorMap.
-    idxs_to_keep = []
-    for idx, key in enumerate(input.keys):
-
-        # Skip the block for l=5, hydrogen
-        if (
-            key[key_idxs["spherical_harmonics_l"]] == 5
-            and key[key_idxs["species_center"]] == 1
-        ):
-            continue
-
-        # Skip the blocks with parity = -1
-        if key[key_idxs["inversion_sigma"]] == -1:
-            continue
-
-        # Store the index of the key to keep if passed the above conditionss
-        idxs_to_keep.append(idx)
-
-    # Keep only the key names indicating lambda and species, i.e. drop the
-    # 'order_nu' and 'inversion_sigma'
-    new_names = ["spherical_harmonics_l", "species_center"]
-
-    # Slice the keys Labels object to generate keys of blocks to keep
-    new_keys = Labels(
-        names=new_names,
-        values=np.array(
-            [
-                [k[key_idxs[new_names[0]]], k[key_idxs[new_names[1]]]]
-                for k in input.keys[idxs_to_keep]
-            ]
-        ),
-    )
-    new_blocks = [input[k].copy() for k in input.keys[idxs_to_keep]]
-
-    # Construct and return the cleaned TensorMap
-    return TensorMap(keys=new_keys, blocks=new_blocks)
-
-
-def recombine_coulomb_matrices(data_dir: str, num_sub_matrices: int):
+def recombine_coulomb_metrics(data_dir: str):
     """
     The TensorMap of coulomb matrices for the azoswitch workflow example is too
     large to store as a single file in the GitHub repository. As such, the
@@ -69,10 +20,10 @@ def recombine_coulomb_matrices(data_dir: str, num_sub_matrices: int):
     """
     # Load the individual TensorMaps into a list
     cm_list = [
-        io.load(
-            os.path.join(data_dir, "coulomb_matrices/", f"coulomb_matrices_{i}.npz")
+        equistore.load(
+            os.path.join(data_dir, "coulomb_metrics/", f"coulomb_metrics_{i}.npz")
         )
-        for i in range(num_sub_matrices)
+        for i in range(6)
     ]
 
     # Combine them
@@ -90,30 +41,6 @@ def recombine_coulomb_matrices(data_dir: str, num_sub_matrices: int):
         keys=combined_keys,
         blocks=combined_blocks,
     )
-    io.save(os.path.join(data_dir, "coulomb_matrices.npz"), combined_tensor)
+    equistore.save(os.path.join(data_dir, "coulomb_metrics.npz"), combined_tensor)
 
     return combined_tensor
-
-def drop_structure_label(tensor: TensorMap):
-    """
-    Drops 'structure' from the samples Labels of each block in the input
-    ``tensor`` and returns a new TensorMap.
-    """
-    new_blocks = []
-    for key in tensor.keys:
-        new_block = TensorBlock(
-            samples=Labels(
-                names=[
-                    "center",
-                ],
-                values=tensor[key].samples["center"].reshape(-1, 1),
-            ),
-            components=[c for c in tensor[key].components],
-            properties=tensor[key].properties,
-            values=tensor[key].values,
-        )
-        new_blocks.append(new_block)
-    return TensorMap(
-        keys=tensor.keys,
-        blocks=new_blocks,
-    )
